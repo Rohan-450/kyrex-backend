11b8b9682ad8c3098b45bf38d3fc2f8400b2ab86
Kaushik Chowdhury <kaushikchowdhury231@gmail.com>
2025-02-23 13:07:54 +0530

    Added cros

--- ponder.config.ts
+++ ponder.config.ts
@@ -1,14 +1,25 @@
-
-
 import { createConfig } from "ponder";
 import { http } from "viem";
 import { AppealContract } from "./abis/AppealContract";
 
 export default createConfig({
-  //  database: {
-  //   kind: "postgres",
-  //   connectionString: process.env.DATABASE_URL
-  // },
+  database: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  },
+  http: {
+    cors: {
+      origin: [
+        "https://kyrexx-darkard2003-darks-projects-dcee960a.vercel.app",
+        "http://localhost:3000"
+      ],
+      methods: ["GET", "POST"],
+      allowedHeaders: ["Content-Type"],
+      credentials: true
+    }
+  },
   networks: {
     amoy: {
       chainId: 80002,
